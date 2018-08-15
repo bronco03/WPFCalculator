d22cc59f237bc34fd43369010962d1c09e131561
Dávid <Dvid@DVID-PC>
2018-08-15 13:03:18 +0200

    base program

---
+++ WPFCalculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            if(Tb.Text.Equals("0")) Tb.Text = ((Button)sender).Content.ToString();
+            else Tb.Text += ((Button)sender).Content;
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+           // Tb.Text += ((Button)sender).Content;
+        }
+    }
+}
