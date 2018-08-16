7237fd4eeeadbe0b5797768edd054ffdfc94276b
Dávid <Dvid@DVID-PC>
2018-08-16 14:54:51 +0200

    up3

--- WPFCalculator/MainWindow.xaml.cs
+++ WPFCalculator/MainWindow.xaml.cs
@@ -24,16 +24,59 @@ namespace WPFCalculator
         {
             InitializeComponent();
         }
-
+        List<string> numbers = new List<string>();
+        List<string> operators = new List<string>();
+        string actualNumber = "0";
         private void Number_Click(object sender, RoutedEventArgs e)
         {
-            if(Tb.Text.Equals("0")) Tb.Text = ((Button)sender).Content.ToString();
-            else Tb.Text += ((Button)sender).Content;
+            
+            if (Tb.Text.Equals("0"))
+            {
+                actualNumber = ((Button)sender).Content.ToString();
+                Tb.Text = ((Button)sender).Content.ToString();
+
+            }
+            else
+            {
+                actualNumber = actualNumber + ((Button)sender).Content.ToString();
+                Tb.Text += ((Button)sender).Content.ToString();
+            }
+
         }
 
         private void Operator_Click(object sender, RoutedEventArgs e)
         {
-            Tb.Text += ((Button)sender).Content;
+            numbers.Add(actualNumber);
+            operators.Add(((Button)sender).Content.ToString());
+            Tb.Text = "0";
+
+        }
+
+        private void Result_Click(object sender, RoutedEventArgs e)
+        {
+            numbers.Add(actualNumber);
+            int i = 0;
+            int act = int.Parse(numbers[i]);
+            foreach (string a in operators)
+            {
+                i++;
+                switch (a) {
+
+                    case "+": act = act + int.Parse(numbers[i]);
+                        break;
+                    case "-":
+                        act = act - int.Parse(numbers[i]);
+                        break;
+                    case "*":
+                        act = act * int.Parse(numbers[i]);
+                        break;
+                    case "/":
+                        act = act / int.Parse(numbers[i]);
+                        break;
+                }
+                
+            }
+            Tb.Text = act.ToString();
         }
     }
 }
